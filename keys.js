c839eebde865ba04bf637412112587ce2817ae04
Henrique Costa Pessoa <hcostapessoa@gmail.com>
2020-02-19 17:19:20 +0000

    fifth commit

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://henriquecostapessoa:1234@mytinerary-bpyb8.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://henriquecostapessoa:1234@mytinerary-bpyb8.mongodb.net/mytinerary_cities?retryWrites=true&w=majority",
 };
--- routes/cities.js
+++ routes/cities.js
@@ -8,3 +8,14 @@ router.get('/test', (req, res) => {
 })
 module.exports = router
 
+const cityModel = require('../model/cityModel')
+
+/*get all cities*/
+router.get('/all',
+    (req, res) => {
+        cityModel.find({})
+            .then(files => {
+                res.send(files)
+            })
+            .catch(err => console.log(err));
+    });
